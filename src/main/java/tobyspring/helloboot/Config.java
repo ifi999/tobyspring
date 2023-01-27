7452182a03cf211b73b68a8178907db5b998cd8a
ifi999 <kkwkjm4231@gmail.com>
2023-01-27 17:25:49 +0900

    add: 합성 애노테이션의 적용

---
+++ src/main/java/tobyspring/helloboot/Config.java
@@ -0,0 +1,20 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration // @Component를 먼저 적었었는데, @Configuration도 meta annotation으로 @Component를 가지고 있음
+public class Config {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+}
