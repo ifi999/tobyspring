80c254518b59579997a63d12068cb93b5aa4936f
ifi999 <kkwkjm4231@gmail.com>
2023-02-07 11:42:02 +0900

    스프링 부트의 자동 구성과 테스트로 전환

--- src/test/java/tobyspring/helloboot/HelloServiceCountTest.java
+++ src/test/java/tobyspring/helloboot/HelloServiceCountTest.java
@@ -1,16 +1,17 @@
 package tobyspring.helloboot;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@HellobootTest
+@Transactional
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class HelloServiceCountTest {
 
     @Autowired
